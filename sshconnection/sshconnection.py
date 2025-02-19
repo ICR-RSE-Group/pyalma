203df23786dbced385b5c0eb7ad5cc9a93d3a1ae
Mira <mira.sarkis@icr.ac.uk>
2025-02-19 16:04:46 +0000

    Use polymorphism for local/remote file/cmd reading/executing

--- sshconnection/sshconnection.py
+++ sshconnection/sshconnection.py
@@ -1,195 +1,196 @@
 import os
-import sys
-import pandas as pd
 import paramiko
 import logging
-from io import StringIO, BytesIO
 import argparse
+import pandas as pd
+from io import StringIO
 from stat import S_ISDIR, S_ISREG
 import pysam
 import tempfile 
 
-dir_path = os.path.dirname(os.path.realpath(__file__))
-sys.path.append(dir_path)
-__file__
-
 # Configure logging for better traceability
 logging.basicConfig(level=logging.DEBUG)
 
-def main():
-    parser = argparse.ArgumentParser(description="SSH Command Runner")
-    parser.add_argument('source', choices=['local', 'remote'], help="Source of the command execution (local or remote)")
-    parser.add_argument('command', help="The command to run")
-    parser.add_argument('--username', help="Username for the SSH server, required with remote")
-    parser.add_argument('--password', help="Password for the SSH server, required with remote if no private keys are available")
-    parser.add_argument('--server', default="alma.icr.ac.uk", help="Remote server address (default: alma.icr.ac.uk)")
-    parser.add_argument('--df', action='store_true', help="Return the output as pandas DataFrame if set")
-    parser.add_argument('--sep', default=',', help="Use selected separator when reading from csv")
-
-    args = parser.parse_args()
- 
-    # Check for valid authentication method (either password or private_key)
-    if args.source=="remote":
-        if not args.password :
-            raise ValueError("You must specify either --password remote connections.")
-    # Create SSH connection based on authentication method
-    ssh = SshConnection(args.source, args.server, username=args.username, password=args.password)
+class FileReader:
+    """Base class for reading files."""
+    def read_file(self, path):
+        raise NotImplementedError("Subclasses must implement read_file")
     
-    result = ssh.run_cmd(cmd=args.command, is_string=not args.df, sep=args.sep)
+    def listdir(self, path):
+        raise NotImplementedError("Subclasses must implement listdir")
 
-    # Handle unpacking based on length
-    if len(result) == 2:
-        output, err = result
-    else:
-        output, err = result, None
+    def read_file_into_df(self, path, type, sep=",", header=None, colnames=[], on_bad_lines='skip'):
+        raise NotImplementedError("Subclasses must implement read_file_into_df")
 
-    if err:
-        print(f"Error: {err}")
-    else:
-        print(output)
-
-class SshConnection:
-    def __init__(self, source, server="alma-app.icr.ac.uk", username=None, password=None):
-        """
-        Initializes the SSH connection instance.
-
-        :param source: could be 'local' or 'remote'
+class LocalFileReader(FileReader):
+    """Reads local files and executes local commands."""
+    def read_file(self, path):
+        try:
+            with open(path, 'r') as file:
+                return file.read()
+        except Exception as e:
+            logging.error(f"Error reading local file {path}: {e}")
+            return None
+    
+    def listdir(self, path):
+        try:
+            with os.scandir(path) as entries:
+                files = [entry.name for entry in entries if entry.is_file()]
+                #for some reason entries empty after this loop
+            with os.scandir(path) as entries:
+                dirs =  [entry.name for entry in entries if entry.is_dir()]
+                print(dirs)
+            return dirs, files
+        except Exception as e:
+            logging.error(f"Error listing directory {path}: {e}")
+            return []
+    
+    def run_cmd(self, command):
+        try:
+            result = os.popen(command).read()
+            print(result)
+            return {"output": result, "err": None}
+        except Exception as e:
+            logging.error(f"Error executing command {command}: {e}")
+            return {"output": None, "err": str(e)}
+    def read_file_into_df(self, path, type, sep=",", header=None, colnames=[], on_bad_lines='skip'):
+        try:
+            if type == "vcf":
+                return read_vcf_file_into_df(path)
+            return pd.read_csv(path, sep=sep, header=header, names=colnames, on_bad_lines=on_bad_lines)
+        except Exception as e:
+            logging.error(f"Error reading local file into DataFrame {path}: {e}")
+            return None
+class SshConnection(FileReader):
+    """Initializes the SSH connection instance.
         :param username: SSH username for alma
         :param password: SSH password for alma
         :param server: The remote server address (default is 'alma.icr.ac.uk')
-        """
-        self.source = source.strip()
+    """
+    #use alma-app since it has an sftp server
+    def __init__(self, server="alma-app.icr.ac.uk", username=None, password=None):
         self.server = server.strip()
         self.username = username.strip() if username else None
         self.password = password.strip() if password else None
-        
-    def run_cmd(self, cmd, is_string=True, sep=","):
-        """
-        Runs a command either locally or remotely via SSH.
-
-        :param cmd: Command to run on local or remote server
-        :param return_dataframe: Boolean flag to determine if result should be returned as pandas DataFrame
-        :return: Command output as string or pandas DataFrame
-        """
-        if self.source == "local":
-            return self.run_local(cmd)#add support for listdir and csv reading
-        else:
-            return self.run_remote(cmd, is_string, sep)
-
-    def run_local(self, cmd):
-        """
-        Executes a command locally.
-
-        :param cmd: Command to execute locally
-        :return: Command output as string
-        """
+    
+    def _connect(self):
+        client = paramiko.SSHClient()
+        client.load_system_host_keys()
+        client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        client.connect(self.server, username=self.username, password=self.password, timeout=30)
+        return client
+    
+    def read_file(self, path):
         try:
-            logging.info(f"Running local command: {cmd}")
-            output = os.popen(cmd).read()
-            return output
+            client = self._connect()
+            sftp = client.open_sftp()
+            with sftp.file(path, 'r') as file:
+                content = file.read().decode()
+            sftp.close()
+            client.close()
+            return content
         except Exception as e:
-            logging.error(f"Error running local command: {e}")
-            return str(e)
-        
-    #by default list all directories within a path
+            logging.error(f"Error reading SSH file {path}: {e}")
+            return None
+    
     def listdir(self, path):
         try:
-            with paramiko.SSHClient() as client:
-                client.load_system_host_keys()
-                client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-                client.connect(self.server, username=self.username, password=self.password, timeout=30)
-                #Use SFTP to retrieve the file as a Dataframe. sftp is not working for listdir
-                with client.open_sftp() as sftp:
-                    directories = []
-                    files = []
-                    # List directory contents with attributes
-                    for entry in sftp.listdir_attr(path):
-                        mode = entry.st_mode
-                        if S_ISDIR(mode):
-                            directories.append(entry.filename)
-                        elif S_ISREG(mode):
-                            files.append(entry.filename)
-                    return directories, files   
+            client = self._connect()
+            sftp = client.open_sftp()
+            files = sftp.listdir(path)
+            directories = []
+            files = []
+            # List directory contents with attributes
+            for entry in sftp.listdir_attr(path):
+                mode = entry.st_mode
+                if S_ISDIR(mode):
+                    directories.append(entry.filename)
+                elif S_ISREG(mode):
+                    files.append(entry.filename)
+            sftp.close()
+            client.close()
+            return directories, files 
         except Exception as e:
-            logging.error(f"Error running remote command: {e}")
-            return "", str(e)
-    def read_vcf_file_into_df(self, local_path):
-        vcf_in = pysam.VariantFile(local_path)
-        return vcf_in
-    def decode_file_by_type(self, file_content, type, sep=",", header=None, colnames=[], on_bad_lines='skip'):
-        if type in ["csv", "tsv", "bed"]:
-            return pd.read_csv(StringIO(file_content.decode('utf-8')), sep=sep, header=header, names=colnames, on_bad_lines=on_bad_lines)
-        # if type in ["vcf"]: # replaced by a download to a local folder
-        #     vcf_buffer = BytesIO(file_content)
-        #     # Use pysam to read the VCF data from the buffer
-        #     return pysam.VariantFile(vcf_buffer, 'r')#read binary
-        else:
-            print("file type not supported")
-        
-    # read any file of any type, locally or remotely
-    # return a dataframe
-    #note: need to think if I should keep the connection opened?! 
-    def read_file_into_df(self, path, type, sep=",", header=None, colnames=[], on_bad_lines='skip'):
-        try:
-            with paramiko.SSHClient() as client:
-                client.load_system_host_keys()
-                client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-                client.connect(self.server, username=self.username, password=self.password, timeout=30)
-                #Use SFTP to retrieve the file as a Dataframe. sftp is not working for listdir
-                with client.open_sftp() as sftp:
-                    if type in ["vcf"]:# option under testing
-                        with tempfile.TemporaryDirectory() as tmpdirname:
-                            print('created temporary directory', tmpdirname)
-                            local_file = tmpdirname+"tmp.vcf"
-                            sftp.get(path, local_file)#download file locally
-                            return self.read_vcf_file_into_df(local_file)
-
-                    #reading files
-                    with sftp.open(path, 'r') as remote_file:
-                        file_content = remote_file.read()
-                        return self.decode_file_by_type(file_content, type, sep, header, colnames, on_bad_lines)
-        except Exception as e:
-            logging.error(f"Error running remote command: {e}")
-            return "", str(e)               
-
-
-    def run_remote(self, cmd, is_string=True, sep=","):
+            logging.error(f"Error listing SSH directory {path}: {e}")
+            return []
+    
+    def run_cmd(self, command):
         """
         Executes a command on the remote server via SSH.
 
         :param cmd: Command to execute remotely
-        :param is_string: Boolean flag to determine if result should be returned as a string or pandas DataFrame
-        :param sep: String to use as a separator for csv files
-        :return: Command output as string or pandas DataFrame
+        :return: Command output as string always, since file reading 
+                 is another command
         """
         try:
-            logging.info(f"Running remote command: {cmd}")
-            with paramiko.SSHClient() as client:
-                # Load existing host keys from the user's known_hosts file
-                # client.connect start checking keys before trying to connect via password
-                client.load_system_host_keys()
-                client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-                client.connect(self.server, username=self.username, password=self.password, timeout=30)
-
-                if not is_string:
-                    #Use SFTP to retrieve the file as a Dataframe. sftp is not working for listdir
-                    with client.open_sftp() as sftp:
-                        #reading files
-                        with sftp.open(cmd, 'r') as remote_file:
-                            file_content = remote_file.read().decode('utf-8')
-                            df = pd.read_csv(StringIO(file_content), sep=sep)
-                            return df, ""
-                else:
-                    # Return the command output as a string
-                    _, stdout, stderr = client.exec_command(cmd, get_pty=True)
-                    err_str = stderr.read().decode("ascii")
-                    out_str = stdout.read().decode("ascii")
-
-                    return out_str, err_str
-
+            client = self._connect()
+            _, stdout, _ = client.exec_command(command)
+            output = stdout.read().decode("ascii")
+            client.close()
+            return {"output":output,"err":None}
         except Exception as e:
-            logging.error(f"Error running remote command: {e}")
-            return "", str(e)
+            logging.error(f"Error executing SSH command {command}: {e}")
+            return {"output":None,"err":str(e)}
+
+    def read_file_into_df(self, path, type, sep=",", header=None, colnames=[], on_bad_lines='skip'):
+        try:
+            client = self._connect()
+            #Use SFTP to retrieve the file as a Dataframe. sftp is not working for listdir
+            sftp = client.open_sftp()
+            if type == "vcf":
+                with tempfile.TemporaryDirectory() as tmpdirname:
+                    print('created temporary directory', tmpdirname)
+                    local_file = os.path.join(tmpdirname, "tmp.vcf")
+                    sftp.get(path, local_file)
+                    return read_vcf_file_into_df(local_file)
+            else:
+                with sftp.open(path, 'r') as remote_file:
+                    file_content = remote_file.read()
+                    return decode_file_by_type(file_content, type, sep=sep, header=header, colnames=colnames, on_bad_lines=on_bad_lines)
+        except Exception as e:
+            logging.error(f"Error reading SSH file into DataFrame {path}: {e}")
+            return None
+        
+def decode_file_by_type(content, type, sep=",", header=None, colnames=[], on_bad_lines='skip'):
+    """Decodes file content based on type."""
+    if type in ['csv', 'tsv', 'bed']:
+        return pd.read_csv(StringIO(content.decode('utf-8')), sep=sep, header=header, names=colnames, on_bad_lines=on_bad_lines)
+        #elif type == 'vcf':
+        #    return pd.read_csv(StringIO(content), comment='#', delimiter='\t')
+    else:
+        return content
+def read_vcf_file_into_df(path):
+    vcf_in = pysam.VariantFile(path)
+    return vcf_in
+
+def main():
+    parser = argparse.ArgumentParser(description="File Reader")
+    parser.add_argument("--local", help="Path to local file", type=str)
+    parser.add_argument("--ssh", help="Path to remote file", type=str)
+    parser.add_argument("--host", help="SSH server", type=str)
+    parser.add_argument("--user", help="SSH Username", type=str)
+    parser.add_argument("--password", help="SSH Password", type=str)
+    parser.add_argument("--cmd", help="Command to execute", type=str)
     
+    args = parser.parse_args()
+    
+    if args.local:
+        reader = LocalFileReader()
+        content = reader.read_file(args.local)
+        if content:
+            print("Local File Content:\n", content)
+    elif args.ssh and args.host and args.user and args.password:
+        reader = SshConnection(args.host, args.user, args.password)
+        content = reader.read_file(args.ssh)
+        if content:
+            print("Remote File Content:\n", content)
+    elif args.cmd:
+        reader = LocalFileReader()
+        output = reader.run_cmd(args.cmd)
+        if output:
+            print("Command Output:\n", output)
+    else:
+        print("Invalid arguments. Provide either --local or --ssh with SSH details, or --cmd for command execution.")
+
 if __name__ == "__main__":
     main()

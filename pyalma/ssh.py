825a9e524530cf407daab9650e1603671d2d26d3
Mira <mira.sarkis@icr.ac.uk>
2025-04-04 09:53:09 +0100

    add if

--- pyalma/ssh.py
+++ pyalma/ssh.py
@@ -58,11 +58,15 @@ class SshClient(FileReader):
     def run_cmd(self, command):
         try:
             stdin, stdout, stderr = self.ssh_client.exec_command(command)
+            print(f"✅: {stdin}, {stdout}, {stderr}" )
             output = stdout.read().decode("ascii")
-            filtered_output = "\n".join(
-                line for line in output.split("\n") 
-                if not any(line.startswith(_filter) for _filter in self.filtered_patterns["filters"])
-            )
+            print(f"✅: {output}")
+            filtered_output = ""
+            if output != "": 
+                filtered_output = "\n".join(
+                    line for line in output.split("\n") 
+                    if not any(line.startswith(_filter) for _filter in self.filtered_patterns["filters"])
+                )
             return {"output": filtered_output, "err": None}
         except Exception as e:
             logging.error(f"❌ [run_cmd]: Error executing SSH command {command}: {e}")

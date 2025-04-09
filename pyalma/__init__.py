d8b6f863eb637477cd76cd3d0ece5150e889136d
Mira <mira.sarkis@icr.ac.uk>
2025-04-09 15:51:33 +0100

    setup pyalma to include version dynamically based on tags

--- pyalma/__init__.py
+++ pyalma/__init__.py
@@ -3,3 +3,9 @@ from .cli import main
 from .local import LocalFileReader
 from .ssh import SshClient
 from .pdfreader import read_pdf_to_dataframe,read_pdf_as_text
+from importlib.metadata import version, PackageNotFoundError
+
+try:
+    __version__ = version("pyalma")
+except PackageNotFoundError:
+    __version__ = "unknown"

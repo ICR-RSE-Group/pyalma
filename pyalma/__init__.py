a70333911a4afdc5914d658d3c3878696b7850ea
Rachel Alcraft <rachel.alcraft@icr.ac.uk>
2025-04-26 18:01:28 +0100

    Add SshClosed to api

--- pyalma/__init__.py
+++ pyalma/__init__.py
@@ -2,6 +2,7 @@
 from .cli import main 
 from .local import LocalFileReader
 from .ssh import SshClient
+from .ssh_closed import SshClientClosed
 from .pdfreader import read_pdf_to_dataframe,read_pdf_as_text
 from importlib.metadata import version, PackageNotFoundError
 

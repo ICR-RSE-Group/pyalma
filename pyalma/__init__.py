c51925cd1c82dd8c71dccf3816b1a2ea153988c5
Rachel Alcraft <rachel.alcraft@icr.ac.uk>
2025-04-28 09:24:47 +0100

    removed ssh_closed from imports

--- pyalma/__init__.py
+++ pyalma/__init__.py
@@ -2,7 +2,6 @@
 from .cli import main 
 from .local import LocalFileReader
 from .ssh import SshClient
-from .ssh_closed import SshClientClosed
 from .pdfreader import read_pdf_to_dataframe,read_pdf_as_text
 from importlib.metadata import version, PackageNotFoundError
 

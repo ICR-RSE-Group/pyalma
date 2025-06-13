79e0161add0dda45038a573b39f6777e6a81297e
ashcherbakova <stacy.shcherbakova@icr.ac.uk>
2025-06-13 17:29:38 +0100

    Fix test

--- tests/test_debug.py
+++ tests/test_debug.py
@@ -1,5 +1,13 @@
 import logging
+import pytest
 from pyalma.debug import setup_paramiko
+
+@pytest.fixture
+def paramiko_logger():
+    logger = logging.getLogger("paramiko")
+    original_level = logger.level
+    yield logger
+    logger.setLevel(original_level)
 
 def test_setup_paramiko_debug_true(paramiko_logger):
     setup_paramiko(debug=True)

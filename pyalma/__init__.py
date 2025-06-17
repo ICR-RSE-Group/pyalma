90abd0e12fe8c06ed58ad1be7b56bbf5d9e96ef5
ashcherbakova <stacy.shcherbakova@icr.ac.uk>
2025-06-17 10:36:00 +0100

    Fixed logger issue

--- pyalma/__init__.py
+++ pyalma/__init__.py
@@ -5,6 +5,9 @@ from .ssh import SshClient
 from .securessh import SecureSshClient
 from .pdfreader import read_pdf_to_dataframe,read_pdf_as_text
 from importlib.metadata import version, PackageNotFoundError
+from pyalma.debug import setup_paramiko
+
+setup_paramiko(debug=False)
 
 try:
     __version__ = version("pyalma")
--- pyalma/debug.py
+++ pyalma/debug.py
@@ -1,8 +1,7 @@
 import logging
 
-paramiko_logger = logging.getLogger("paramiko")
-paramiko_logger.setLevel(logging.WARNING)
-
 def setup_paramiko(debug=False):
+    paramiko_logger = logging.getLogger("paramiko")
+    paramiko_logger.setLevel(logging.WARNING)
     if debug:
         paramiko_logger.setLevel(logging.DEBUG)
